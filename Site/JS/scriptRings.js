51bcc5d43c389f8166d1691d4a8de6369dbe1d57
Redeem <121949352+Captain-Redeem@users.noreply.github.com>
2025-06-30 19:08:46 +0100

    Add files via upload

---
+++ Site/JS/scriptRings.js
@@ -0,0 +1,70 @@
+(function () {
+    "use strict";
+    
+    var delivery = document.getElementById('del-to');
+
+    document.addEventListener('DOMContentLoaded', function() {
+document.getElementById('cart').addEventListener('submit', estimateTotal);
+    
+        var btnEstimate = document.getElementById('btn-estimate');
+
+        btnEstimate.disabled = true;
+        
+        delivery.addEventListener('change', function () {
+            
+            if (delivery.value === '') {
+                btnEstimate.disabled = true;
+            } else {
+                btnEstimate.disabled = false;
+            }
+
+        });
+    });
+        
+    function estimateTotal(event) {
+        event.preventDefault();
+        
+        if (delivery.value === '') {
+            alert('Please pick a delivery option');
+            
+            state.focus();
+        }
+        
+        var Item01 = parseInt(document.getElementById('product01').value, 10) || 0,
+            Item02 = parseInt(document.getElementById('product02').value, 10) || 0,
+            Item03 = parseInt(document.getElementById('product03').value, 10) || 0,
+            Item04 = parseInt(document.getElementById('product04').value, 10) || 0, 
+            Item05 = parseInt(document.getElementById('product05').value, 10) || 0, 
+            Item06 = parseInt(document.getElementById('product06').value, 10) || 0, 
+            deliveryState = delivery.value;
+        
+        var totalQty =Item01 + Item02 + Item03 + Item04 + Item05 + Item06,
+            deliveryCharge = 0,
+            minCharge = 99.99,
+            taxFactor = 1,
+            estimate,
+        totalItemPrice = (40 * Item01) + (75 * Item02) + (50 * Item03) + (65 * Item04) + (45 * Item05) + (95 * Item06);
+        
+        if (deliveryState === 'USA') {
+            taxFactor = 1.075;
+        }
+        
+        if (deliveryState === 'EU') {
+            taxFactor = 1.050;
+        }
+        
+        if (totalItemPrice < minCharge) {
+            deliveryCharge = 10;
+        }
+        
+        estimate = '£' + ((totalItemPrice * taxFactor) + deliveryCharge).toFixed(2);
+        
+        document.getElementById('txt-estimate').value = estimate;
+        
+        var results = document.getElementById('results');
+        
+        results.innerHTML = 'Total items ordered: ' + totalQty + '<br />';
+        results.innerHTML += 'Delivery charges: £' + deliveryCharge.toFixed(2) + '<br />';
+        results.innerHTML += 'Tax Paid: ' + ((taxFactor -1) * 100).toFixed(2) + '%(' + deliveryState +')';
+    }
+}());
---
+++ Site/login.js
@@ -0,0 +1,40 @@
+var count = 2;
+
+function validate() {
+	var un = document.myform.username.value;
+	var pw = document.myform.pword.value;
+	var valid = false;
+	var unArray = ["Melinda", "Thor"]; // As many as you like, no comma after final entry
+	var pwArray = ["Marvel", "Marvel"]; // Number of passwords must equal number of users
+
+	for (var i = 0; i < unArray.length; i++) {
+		if ((un == unArray[i]) && (pw == pwArray[i])) {
+			valid = true;
+			break;
+		}
+	}
+
+	if (valid) {
+		alert("Login Successful");
+		window.location = "members.html";
+		return false;
+	}
+
+	var t = " tries";
+	if (count == 1) {
+		t = " try";
+	}
+	if (count >= 1) {
+		alert("Invalid username and/or password.  You have " + count + t + " left.");
+		document.myform.username.value = "";
+		document.myform.pword.value = "";
+		count--;
+	} else {
+		alert("Still incorrect! You have no more attempts left");
+		document.myform.username.value = "No more attempts allowed";
+		document.myform.pword.value = "";
+		document.myform.username.disabled = true;
+		document.myform.pword.disabled = true;
+		return false;
+	}
+}
